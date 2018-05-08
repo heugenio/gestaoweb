018cb34151634ae862d31cb291b27fd13433fe55
Daniel Filho <danielederfilho@gmail.com>
2018-05-08 13:56:10 -0300

    Criação do projeto GestãoWeb

---
+++ src/main/java/com/reviravollta/gestaoweb/GestaowebApplication.java
@@ -0,0 +1,12 @@
+package com.reviravollta.gestaoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaowebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/reviravollta/gestaoweb/GestaowebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.reviravollta.gestaoweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GestaowebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
