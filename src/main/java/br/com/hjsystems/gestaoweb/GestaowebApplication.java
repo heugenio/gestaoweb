649d7700e3846e1735e1ce88c836a3e914de4fb3
Daniel Filho <danielederfilho@gmail.com>
2018-05-08 16:49:13 -0300

    Iniciando a criação do layout

--- src/main/java/br/com/hjsystems/gestaoweb/GestaowebApplication.java
+++ src/main/java/br/com/hjsystems/gestaoweb/GestaowebApplication.java
@@ -2,8 +2,10 @@ package br.com.hjsystems.gestaoweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = {br.com.hjsystems.gestaoweb.controller.IndexController.class})
 public class GestaowebApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/hjsystems/gestaoweb/controller/IndexController.java
@@ -0,0 +1,15 @@
+package br.com.hjsystems.gestaoweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+		
+	@RequestMapping("/")
+    public String indexTemplate(Model model) {
+        return "page-blank";
+    }
+	
+}
